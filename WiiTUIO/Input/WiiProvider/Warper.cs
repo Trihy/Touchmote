b7427bfea92221a5d94af826edc4942a0dfd5927
Travis Nickles <ryochan7@protonmail.com>
2025-07-21 22:14:34 -0500

    Invert destination Y coor in warper

--- WiiTUIO/Input/WiiProvider/Warper.cs
+++ WiiTUIO/Input/WiiProvider/Warper.cs
@@ -37,7 +37,7 @@ namespace WiiTUIO.Provider
             center[0] = (float)this.settings.CenterX;
             center[1] = (float)this.settings.CenterY;
             if (Settings.Default.pointer_4IRMode == "square")
-                setDestination(this.settings.TRled, 1.0f, this.settings.TLled, 1.0f, this.settings.TLled, 0.0f, this.settings.TRled, 0.0f);
+                setDestination(this.settings.TRled, 0.0f, this.settings.TLled, 0.0f, this.settings.TLled, 1.0f, this.settings.TRled, 1.0f);
             else if (Settings.Default.pointer_4IRMode == "diamond")
                 setDestination(1.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.5f, 0.5f, 0.0f);
         }
